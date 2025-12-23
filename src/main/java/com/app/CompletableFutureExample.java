3c85bf333c510b588c0c1cdeed8271d0cf2a1003
Tamannaur <tamannaur.aktar@gmail.com>
2025-12-23 19:48:50 +0530

    added code

---
+++ src/main/java/com/app/CompletableFutureExample.java
@@ -0,0 +1,28 @@
+package com.app;
+
+import java.util.concurrent.CompletableFuture;
+
+public class CompletableFutureExample {
+    public static void main(String[] args) {
+        CompletableFuture.supplyAsync(()->"Tamannaur")
+                .thenApply(val-> val+" Aktar")
+                .thenApply(val-> val + " Hussain")
+                .exceptionally(Throwable::getMessage) //.exceptionally(e->e.getMessage())
+                .thenAccept(System.out::println);
+
+
+        //
+
+        CompletableFuture<String> s1 = CompletableFuture.supplyAsync(() -> "Tamannaur")
+                .thenApply(val -> val + " Aktar");
+
+        CompletableFuture<String> s2 = CompletableFuture.supplyAsync(() -> " Hussain");
+
+        CompletableFuture<String> res = s1.thenCombine(s2, (val1, val2) -> val1 + val2);
+        res.exceptionally(Throwable::getMessage);
+        System.out.println(res.join());
+
+        //
+
+    }
+}
---
+++ src/main/java/com/app/ExecutorServiceExample.java
@@ -0,0 +1,20 @@
+package com.app;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ExecutorServiceExample {
+    public static void main(String[] args)  {
+        ExecutorService e = Executors.newFixedThreadPool(5);
+        try {
+            Future<String> res = e.submit(() -> "Result");
+            System.out.println(res.get());
+        }catch (InterruptedException | ExecutionException ex) {
+            ex.printStackTrace();
+        }finally {
+            e.shutdown();
+        }
+    }
+}
