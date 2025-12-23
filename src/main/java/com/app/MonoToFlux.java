31c8335e250ec0f5686825517fa80855b0769be4
Tamannaur <tamannaur.aktar@gmail.com>
2025-12-23 18:55:07 +0530

    added code

---
+++ src/main/java/com/app/MonoToFlux.java
@@ -0,0 +1,20 @@
+package com.app;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class MonoToFlux {
+    public static void main(String[] args) {
+//        Flux<String> flux = Flux.just("a", "b", "c");
+//        flux.subscribe(System.out::println);
+          func().subscribe(System.out::println);
+    }
+
+    public static Flux<String> func(){
+        Mono<List<String>> res = Mono.just(List.of("a", "b", "c"));
+        //res.flatMapMany(list->Flux.fromIterable(list));
+        return res.flatMapMany(Flux::fromIterable);
+    }
+}
